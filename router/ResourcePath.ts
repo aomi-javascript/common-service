2a3426c6cdfd337654d4ba3b24e601f0a74e5f87
尼古拉斯丶少 <sean.snow@live.com>
2021-06-15 17:04:57 +0800

    feat: 路径服务完善

--- router/ResourcePath.ts
+++ router/ResourcePath.ts
@@ -23,3 +23,36 @@ export type ResourceUri = {
    */
   update?: string
 }
+
+
+/**
+ * 根据base地址直接生成对应的,query,detail,create,update地址
+ * @param path
+ * @param prefix
+ */
+export function setUriFromBase(path: { [key: string]: ResourceUri | string }, prefix: string = '') {
+  Object.keys(path).forEach(key => {
+    const action: ResourceUri | string = path[key];
+    if (typeof action === 'string') {
+      path[key] = `${prefix}${path[key]}`;
+    } else {
+      action.base = `${prefix}${action.base}`;
+      action.query = `${action.base}/query`;
+      action.detail = `${action.base}/detail`;
+      action.create = `${action.base}/create`;
+      action.update = `${action.base}/update`;
+      path[key] = action;
+    }
+  });
+}
+
+/**
+ * 生成新增编辑地址，根据基础地址
+ * <code>
+ *   const path = `${base}/action:(create|update)`
+ * </code>
+ * @param base 基础地址
+ */
+export function actionPath(base: string) {
+  return `${base}/:action(create|update)`;
+}
