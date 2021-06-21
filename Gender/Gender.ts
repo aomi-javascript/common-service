d93453ce33b5f3b3c27043d0a2f47af3092554db
尼古拉斯丶少 <sean.snow@live.com>
2021-06-21 11:13:37 +0800

    feat: 性别定义

---
+++ Gender/Gender.ts
@@ -0,0 +1,13 @@
+/**
+ * 性别信息
+ * @author Sean(sean.snow@live.com) create at 2019-05-05
+ */
+export const Gender = {
+  male: 'MALE',
+  female: 'FEMALE'
+};
+
+export const GenderText = {
+  [Gender.male]: '男',
+  [Gender.female]: '女'
+};
