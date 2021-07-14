adf0f84c256c268eaf973d4e74c20dc628646149
尼古拉斯丶少 <sean.snow@live.com>
2021-07-14 21:21:56 +0800

    feat: 发票信息定义

---
+++ FaPiao/FaPiaoType.ts
@@ -0,0 +1,10 @@
+export enum FaPiaoType {
+  /**
+   * 普票
+   */
+  NORMAL = 'NORMAL',
+  /**
+   * 专票
+   */
+  SPECIAL = 'SPECIAL'
+}
---
+++ FaPiao/zh-cn.ts
@@ -0,0 +1,7 @@
+import { FaPiaoType } from './FaPiaoType';
+
+
+export const FaPiaoTypeText = {
+  [FaPiaoType.NORMAL]: '增值税普通发票',
+  [FaPiaoType.SPECIAL]: '增值税专用发票'
+};
