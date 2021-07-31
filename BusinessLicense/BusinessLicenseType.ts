eef41920cec30c42a24c29ead489e32363406266
尼古拉斯丶少 <sean.snow@live.com>
2021-07-31 11:55:00 +0800

    feat: 营业执照类型

---
+++ BusinessLicense/BusinessLicenseType.ts
@@ -0,0 +1,17 @@
+/**
+ * 营业执照类型
+ */
+export enum BusinessLicenseType {
+  /**
+   * 多证合一的营业执照
+   */
+  businessLicenseAllOne = 'BUSINESS_LICENSE_ALL_ONE',
+  /**
+   * 营业执照
+   */
+  businessLicense = 'BUSINESS_LICENSE',
+  /**
+   * 事业单位证书
+   */
+  institution = 'INSTITUTION',
+}
---
+++ BusinessLicense/zh-cn.ts
@@ -0,0 +1,7 @@
+import { BusinessLicenseType } from './BusinessLicenseType';
+
+export const BusinessLicenseTypeText = {
+  [BusinessLicenseType.businessLicenseAllOne]: '统一社会信用代码',
+  [BusinessLicenseType.businessLicense]: '营业执照',
+  [BusinessLicenseType.institution]: '事业单位证书'
+};
