13a2c7f03800bd4eac0372ec02d7f9dff0122066
尼古拉斯丶少 <sean.snow@live.com>
2021-08-06 14:52:58 +0800

    feat: 卡类型信息定义

--- BankCardType/BankCardType.ts
+++ BankCardType/BankCardType.ts
@@ -1,12 +1,10 @@
-export type BankCardType = 'DEBIT_CARD' | 'CREDIT_CARD' | 'SEMI_CREDIT_CARD' | 'PREPAID_CARD'
-
 /**
  * @author 田尘殇Sean(sean.snow@live.com)
  * @date 2017/3/28
  */
-export const bankCardType: Record<string, BankCardType> = {
-  debitCard: 'DEBIT_CARD',
-  creditCard: 'CREDIT_CARD',
-  semiCreditCard: 'SEMI_CREDIT_CARD',
-  prepaidCard: 'PREPAID_CARD'
-};
+export enum BankCardType {
+  DEBIT_CARD = 'DEBIT_CARD',
+  CREDIT_CARD = 'CREDIT_CARD',
+  SEMI_CREDIT_CARD = 'SEMI_CREDIT_CARD',
+  PREPAID_CARD = 'PREPAID_CARD'
+}
---
+++ BankCardType/zh-ch.ts
@@ -0,0 +1,8 @@
+import { BankCardType } from './BankCardType';
+
+export const BankCardTypeText = {
+  [BankCardType.DEBIT_CARD]: '借记卡',
+  [BankCardType.CREDIT_CARD]: '贷记卡',
+  [BankCardType.SEMI_CREDIT_CARD]: '准贷记卡',
+  [BankCardType.PREPAID_CARD]: '预付费卡'
+};
