2c21fe5f14ded7862e10f8ee9c3ce3bada765634
尼古拉斯丶少 <sean.snow@live.com>
2022-05-20 17:16:40 +0800

    feat: 分页排序工具方法

--- Page/Page.ts
+++ Page/Page.ts
@@ -30,3 +30,27 @@ export type PageRequest = {
    */
   sort: string
 }
+
+
+/**
+ * 从一个参数中提取 查询请求参数
+ */
+export function getPageReq(args: Record<string, any>): PageRequest {
+  return {
+    size: args.size || 20,
+    page: args.page || 0,
+    sort: args.sort || ''
+  };
+}
+
+/**
+ * 排序字符串转成对象
+ * @param sortStr 排序字符串
+ */
+export function sortStr2Obj(sortStr: string) {
+  const tmp = sortStr.split(',');
+  const direction = tmp[1].trim();
+  return {
+    [tmp[0].trim()]: direction === 'desc' ? -1 : 1
+  };
+}
