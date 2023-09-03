27c6d4dd82259042800fc612ff9367091a46340c
sean <sean.snow@live.com>
2023-09-03 15:01:11 +0800

    feat: loading

---
+++ src/decorator/loading.ts
@@ -0,0 +1,29 @@
+// 定义装饰器
+/**
+ * loading 装饰器
+ * 方法执行前检查loading变量值是否为true，如果为true，直接return。反之继续执行，结束后设置为false
+ * @param target
+ * @param propertyKey
+ * @param descriptor
+ */
+export function loading(
+  target: any,
+  propertyKey: string,
+  descriptor: PropertyDescriptor & { loading: boolean },
+) {
+  const originalMethod = descriptor.value;
+
+  descriptor.value = async function (...args: any[]) {
+    if (this.loading) {
+      return; // 如果正在加载，直接返回
+    }
+    this.loading = true; // 设置类的loading变量为true
+    try {
+      return await originalMethod.apply(this, args); // 执行原始方法
+    } finally {
+      this.loading = false; // 设置类的loading变量为false
+    }
+  };
+
+  return descriptor;
+}
