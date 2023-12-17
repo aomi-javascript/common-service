96fd934db70c6744123a816cbcf1a41c25d1110f
sean <sean.snow@live.com>
2023-12-17 17:39:40 +0800

    feat: 设置type

--- src/Page/index.ts
+++ src/Page/index.ts
@@ -1,1 +1,9 @@
-export * from './Page';
+import {DEFAULT_PAGE, Page, PageRequest, sortStr2Obj, getPageReq} from './Page.js';
+
+export {
+  Page,
+  PageRequest,
+  DEFAULT_PAGE,
+  sortStr2Obj,
+  getPageReq
+};
